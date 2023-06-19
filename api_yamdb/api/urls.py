5521dc3a760b70f45a6aea4e35e2355adbf2d9d5
Александр Кашигин <kash2608@yandex.ru>
2023-06-19 21:46:45 +0500

    urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -15,7 +15,7 @@ router.register(r'users', UsersViewSet)
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
-    basename='viewsets'
+    basename='reviews'
 )
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
