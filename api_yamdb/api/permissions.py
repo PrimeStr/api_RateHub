e712a47f37c6730ea287a7e7ec2b547920eb49b4
Maxim Golovin <boss@primestr.ru>
2023-06-20 15:11:07 +0500

    Another small PEP fix.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,8 +5,7 @@ class IsSuperuserOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
-                        request.user.is_admin or request.user.is_superuser))
-                )
+                request.user.is_admin or request.user.is_superuser)))
 
 
 class SelfUserOnly(permissions.BasePermission):
@@ -19,17 +18,13 @@ class SelfUserOnly(permissions.BasePermission):
 
 class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user or request.user.is_moderator
-                or request.user.is_admin or request.user.is_superuser
-                )
+                or request.user.is_admin or request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
